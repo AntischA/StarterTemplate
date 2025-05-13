4945ec79bd936037fd1dcce8e69a879835968b97
AntischA <antevidovicmail@gmail.com>
2025-05-13 11:23:16 +0200

    Poruka

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,40 @@
 import 'package:flutter/material.dart';
+
 void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(body: Center(child: Text('Hello Flutter'))));
+    return MaterialApp(
+      title: 'Starter App',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Starter start this app')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.flutter_dash, size: 64, color: Colors.teal),
+              SizedBox(height: 16),
+              Text(
+                'Pozdrav iz Flutter !',
+                style: TextStyle(fontSize: 24),
+              ),
+              Text(
+                'Drugi pozdrav Flutter !',
+                style: TextStyle(fontSize: 30),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  print('Kliknuto!');
+                },
+                child: Text('Klikni mene sada opet dva sssss'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
